8cfc818862fe4522c4259b5f408880287f3726b3
benjjvi <ben.voisey2@icloud.com>
2024-06-17 18:16:10 +0100

    added a few features

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
-import requests
-from bs4 import BeautifulSoup
 import json
 import warnings
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from googletrans import Translator
+
+import requests
 import twikit
+from bs4 import BeautifulSoup
+from translate import Translator
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+from credentials import CREDENTIALS
+
 
 def analyze_headline(title):
     print(f"Analysing {title}")
-    translator = Translator()
+    translator = Translator(
+        provider="mymemory",
+        to_lang="en",
+        from_lang="cy",
+        email=CREDENTIALS["email_for_translation"],
+    )
 
     # Translate title from Welsh to English
-    translated_title = translator.translate(title, src='cy', dest='en').text
+    translated_title = translator.translate(title)
+    print(title)
+    print(translated_title)
 
     sentiment = SentimentIntensityAnalyzer()
     analysis = sentiment.polarity_scores(translated_title)
 
-    score = analysis['compound']
+    score = analysis["compound"]
 
     return score
 
 
 def fetch_news_as_json():
-    url = 'https://newyddion.s4c.cymru/topics/0'
+    url = "https://newyddion.s4c.cymru/topics/0"
 
     # Make a GET request to the URL
     response = requests.get(url)
-    response.encoding = "utf-8"
 
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the content with BeautifulSoup
-        soup = BeautifulSoup(response.content, 'html.parser')
+        soup = BeautifulSoup(response.content, "html.parser")
 
         # Find the script tag with id '__NEXT_DATA__'
-        script_tag = soup.find('script', id='__NEXT_DATA__')
+        script_tag = soup.find("script", id="__NEXT_DATA__")
 
         # Extract JSON data from the script tag if it exists
         if script_tag:
@@ -47,20 +57,27 @@ def fetch_news_as_json():
                     json_data = json.loads(script_content)
 
                     # Extract articles from JSON data
-                    articles = json_data['props']['pageProps']['articles']
+                    articles = json_data["props"]["pageProps"]["articles"]
 
                     if len(articles) < 2:
-                        warnings.warn("Articles contains less than two elements.", UserWarning)
+                        warnings.warn(
+                            "Articles contains less than two elements.", UserWarning
+                        )
                 else:
-                    raise Exception("Script tag with id '__NEXT_DATA__' has no content.")
+                    raise Exception(
+                        "Script tag with id '__NEXT_DATA__' has no content."
+                    )
             except json.JSONDecodeError as e:
                 raise Exception(f"Error decoding JSON: {e}")
         else:
             raise Exception("Script tag with id '__NEXT_DATA__' not found.")
     else:
-        raise Exception(f"Failed to retrieve the web page. Status code: {response.status_code}")
+        raise Exception(
+            f"Failed to retrieve the web page. Status code: {response.status_code}"
+        )
 
     return articles
+
 
 def process_articles(articles):
     new_articles = []
@@ -68,11 +85,11 @@ def process_articles(articles):
         new_article = {
             "title": article["title"].replace("&#039;", "'"),
             "id": article["id"],
-            "weight": analyze_headline(article["title"])
         }
         new_articles.append(new_article)
-    
+
     return new_articles
+
 
 def get_id_history():
     with open("seen.pydict", "r") as file:
@@ -80,14 +97,35 @@ def get_id_history():
 
     return seen
 
-def compare_history_with_new_request():
-    pass
 
-def add_unseen_articles_to_history():
-    pass
+def compare_history_with_new_request(all_articles):
+    history = get_id_history()
+    new_items = []
 
-def tweet():
-    pass
+    ids_seen = {item["id"] for item in history}
+    for item in all_articles:
+        if item["id"] in ids_seen:
+            pass
+        else:
+            new_items.append(item)
+
+    return new_items
+
+
+def add_unseen_articles_to_history(unseen_articles):
+    history = get_id_history()
+    for item in unseen_articles:
+        history.append(item)
+
+    with open("seen.pydict", "w") as file:
+        file.write(str(history))
+
+
+def tweet(individual_story):
+    weight = analyze_headline(individual_story["title"])
+    print(individual_story)
+    print(weight)
+
 
 # run loop
 while True:
@@ -95,8 +133,8 @@ while True:
     prelogged_ids = get_id_history()
 
     # get new items, and remove any already logged
-    articles = fetch_news_as_json()
-    unseen_articles = compare_history_with_new_request()
+    articles = process_articles(fetch_news_as_json())
+    unseen_articles = compare_history_with_new_request(articles)
 
     # add unseen items to history log.
     add_unseen_articles_to_history(unseen_articles)
@@ -104,5 +142,3 @@ while True:
     # tweet! (X it? i hate the new rebranding.)
     for item in unseen_articles:
         tweet(item)
-
-
